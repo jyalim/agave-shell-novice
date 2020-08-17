03edb202a95451677901978e9b28c2fb13655161
Jason Yalim <yalim@asu.edu>
2020-08-17 13:21:21 -0700

    safer gres option

---
+++ data-shell/signal/my_signal_processing_job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -p serial 
+#SBATCH -q normal
+#SBATCH -t 5
+#SBATCH -c 1
+#SBATCH -e signal_processing_job.%j.err
+#SBATCH -o signal_processing_job.%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jyalim+agave+cli+example@asu.edu
+
+# Grab node information if desired (note a lot of this is recorded by
+# slurm already)
+echo hostname: $(hostname)
+echo    nproc: $(nproc)
+echo     free: $(free -h)
+echo      pwd: $(pwd)
+# Purge any loaded modules to ensure consistent working environment
+module purge
+# Load required software
+module load anaconda/py3
+# Diagnostic information
+module list
+which python
+# Put bash into a diagnostic mode that prints commands
+set -x
+# Starting
+echo STARTED: $(date)
+# Run scientific workflow
+python get_fft.py
+# Send output figure to researcher email
+mail -a fft.png -s "fft complete" "${USER}@asu.edu" <<< "SEE ATTACHED"
+# Finished
+echo FINISHED: $(date)
